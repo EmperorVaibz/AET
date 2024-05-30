75e249568725ef4c83afd3205d4ba6a27844a888
EmperorVaibz <65975752+EmperorVaibz@users.noreply.github.com>
2024-05-30 15:43:00 -0700

    Update map.m

--- map.m
+++ map.m
@@ -1,7 +1,13 @@
-latstart=0;
-lonstart=0;
-latend=21;
-lonend=-73;
+%%Cooridinate array
+latstart=53.32055555555556;
+lonstart=-1.7297222222222221;
+latend=53.31861111111111;
+lonend=-1.6997222222222223;
+
+%Calculating the distance
+distance=6371*acos(sin(latstart/(180/pi))*sin(latend/(180/pi))+cos(latstart/(180/pi))*cos(latend/(180/pi))*cos(lonstart/(180/pi)-lonend/(180/pi)));
+
+%Drawing the map
 figure
 geoplot([latstart,latend],[lonstart,lonend],'-*')
 geobasemap streets
