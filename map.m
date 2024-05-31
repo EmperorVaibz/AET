6f5fedc9cd1f846fbb4a391b84144224daf037ee
EmperorVaibz <65975752+EmperorVaibz@users.noreply.github.com>
2024-05-30 17:15:13 -0700

    Update map.m

--- map.m
+++ map.m
@@ -1,15 +1,18 @@
 %%Cooridinate array
 T = readtable("worldcities.csv");
+cityarr=T.Var1;
 T(1:5,1:5)
 
 %%Setting coordinates
 city1="Istanbul";
 city2="Lagos";
+indc1=1;
+indc2=2;
 
-latstart=T(city1,"lat");
-lonstart=T(city1,"lng");
-latend=T(city2,"lat");
-lonend=T(city2,"lng");
+latstart=T{indc1,"lat"};
+lonstart=T{indc1,"lng"};
+latend=T{indc2,"lat"};
+lonend=T{indc2,"lng"};
 
 %Calculating the distance
 distance=6371*acos(sin(latstart/(180/pi))*sin(latend/(180/pi))+cos(latstart/(180/pi))*cos(latend/(180/pi))*cos(lonstart/(180/pi)-lonend/(180/pi)));
