71481d23c1142df9787c87ffd5b2c1ca871304f3
EmperorVaibz <65975752+EmperorVaibz@users.noreply.github.com>
2024-05-30 17:48:17 -0700

    Update map.m
    Map tracks cities on the map using database automatically

--- map.m
+++ map.m
@@ -1,13 +1,26 @@
 %%Cooridinate array
 T = readtable("worldcities.csv");
 cityarr=T.Var1;
+countryarr=T.country;
 T(1:5,1:5)
 
 %%Setting coordinates
 city1="Istanbul";
+country1="Turkey";
 city2="Lagos";
+country2="Nigeria";
 indc1=1;
 indc2=2;
+for k = 1:numel(cityarr)
+    if cityarr{k}==city1&&countryarr{k}==country1
+        indc1=k;
+    end
+end
+for k = 1:numel(cityarr)
+    if cityarr{k}==city2&&countryarr{k}==country2
+        indc2=k;
+    end
+end
 
 latstart=T{indc1,"lat"};
 lonstart=T{indc1,"lng"};
