c82dd438cd78aeb4595e44aa44b90d020a740b41
EmperorVaibz <65975752+EmperorVaibz@users.noreply.github.com>
2024-05-30 17:05:54 -0700

    Reupload of world cities, updated map

--- map.m
+++ map.m
@@ -1,10 +1,15 @@
 %%Cooridinate array
 T = readtable("worldcities.csv");
 T(1:5,1:5)
-latstart=53.32055555555556;
-lonstart=-1.7297222222222221;
-latend=53.31861111111111;
-lonend=-1.6997222222222223;
+
+%%Setting coordinates
+city1="Istanbul";
+city2="Lagos";
+
+latstart=T(city1,"lat");
+lonstart=T(city1,"lng");
+latend=T(city2,"lat");
+lonend=T(city2,"lng");
 
 %Calculating the distance
 distance=6371*acos(sin(latstart/(180/pi))*sin(latend/(180/pi))+cos(latstart/(180/pi))*cos(latend/(180/pi))*cos(lonstart/(180/pi)-lonend/(180/pi)));
